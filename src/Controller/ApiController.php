cf1f84174d0eda2657a7c1ff8d12e5e8a1603ea6
Louis <louishayotte@gmail.com>
2025-01-13 09:38:08 +0100

    feat: Added Api controller

---
+++ src/Controller/ApiController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Controller;
+
+use App\HttpClient\ApiHttpClient;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+class ApiController extends AbstractController
+{
+    #[Route('/users', name: 'users_list')]
+    public function index(ApiHttpClient $apiHttpClient): Response
+    {
+
+        $users = $apiHttpClient->getUsers();
+        return $this->render('users/index.html.twig', [
+            'users' => $users,
+        ]);
+    }
+}
