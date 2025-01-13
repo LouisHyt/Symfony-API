f719476d96d59ef05c9323003976c8cf7282729d
Louis <louishayotte@gmail.com>
2025-01-13 12:02:16 +0100

    feat: Added api Library

--- src/Controller/ApiController.php
+++ src/Controller/ApiController.php
@@ -2,8 +2,9 @@
 
 namespace App\Controller;
 
+use App\Entity\Membre;
 use App\HttpClient\ApiHttpClient;
-use Symfony\Component\HttpFoundation\Request;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -13,10 +14,53 @@ class ApiController extends AbstractController
     #[Route('/users', name: 'users_list')]
     public function index(ApiHttpClient $apiHttpClient): Response
     {
-
         $users = $apiHttpClient->getUsers();
         return $this->render('users/index.html.twig', [
-            'users' => $users,
+            'users' => $users
         ]);
     }
+
+    #[Route('/users/add', name: 'user_add', methods: 'POST')]
+    public function addMembre(EntityManagerInterface $entityManager)
+    {
+
+        $membre = new Membre();
+
+        $formData = [
+            'title' => filter_input(INPUT_POST, 'title', FILTER_SANITIZE_FULL_SPECIAL_CHARS),
+            'last' => filter_input(INPUT_POST, 'last', FILTER_SANITIZE_FULL_SPECIAL_CHARS),
+            'first' => filter_input(INPUT_POST, 'first', FILTER_SANITIZE_FULL_SPECIAL_CHARS),
+            'email' => filter_input(INPUT_POST, 'email', FILTER_SANITIZE_FULL_SPECIAL_CHARS),
+            'phone' => filter_input(INPUT_POST, 'phone', FILTER_SANITIZE_FULL_SPECIAL_CHARS),
+            'picture' => filter_input(INPUT_POST, 'picture', FILTER_SANITIZE_FULL_SPECIAL_CHARS),
+            'streetnumber' => filter_input(INPUT_POST, 'streetnumber', FILTER_SANITIZE_FULL_SPECIAL_CHARS),
+            'streetname' => filter_input(INPUT_POST, 'streetname', FILTER_SANITIZE_FULL_SPECIAL_CHARS),
+            'postcode' => filter_input(INPUT_POST, 'postcode', FILTER_SANITIZE_FULL_SPECIAL_CHARS),
+            'city' => filter_input(INPUT_POST, 'city', FILTER_SANITIZE_FULL_SPECIAL_CHARS),
+            'country' => filter_input(INPUT_POST, 'country', FILTER_SANITIZE_FULL_SPECIAL_CHARS)
+        ];
+
+        if(in_array(null, $formData)){
+            return $this->redirectToRoute("users_list");
+            exit();
+        }
+
+        $membre->setTitle($formData['title']);
+        $membre->setLast($formData['last']);
+        $membre->setFirst($formData['first']);
+        $membre->setEmail($formData['email']);
+        $membre->setPhone($formData['phone']);
+        $membre->setPicture($formData['picture']);
+        $membre->setStreetnumber($formData['streetnumber']);
+        $membre->setStreetname($formData['streetname']);
+        $membre->setPostcode($formData['postcode']);
+        $membre->setCity($formData['city']);
+        $membre->setCountry($formData['country']);
+
+        $entityManager->persist($membre);
+        $entityManager->flush();
+
+        return $this->redirectToRoute("users_list");
+
+    }   
 }
